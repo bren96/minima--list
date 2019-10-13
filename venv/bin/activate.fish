cdd7e8746593f5684f4398ffb5f0853ba6e66e4c
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2019-10-13 10:36:49 -0400

    fix venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/bcullen/Documents/MyFive/myfive_flask/venv"
+set -gx VIRTUAL_ENV "/Users/bcullen/Documents/myfive_flask/venv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
--- venv/bin/activate.xsh
+++ venv/bin/activate.xsh
@@ -28,7 +28,7 @@ def _deactivate(args):
 _deactivate(["nondestructive"])
 aliases["deactivate"] = _deactivate
 
-$VIRTUAL_ENV = r"/Users/bcullen/Documents/MyFive/myfive_flask/venv"
+$VIRTUAL_ENV = r"/Users/bcullen/Documents/myfive_flask/venv"
 
 $_OLD_VIRTUAL_PATH = $PATH
 $PATH = $PATH[:]
