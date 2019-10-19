50a188a5f54a8bbbdf38eed1cf705ddd83e0dbb1
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2019-10-19 11:07:09 -0400

    set database url 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uugoazwybaczwi:7c8526f5010e90bf259421da375f1be793ab5a3f8bcead490b0a02437cbca0b3@ec2-54-243-208-234.compute-1.amazonaws.com:5432/da9779rd0a0i19'
 db = SQLAlchemy(app)
-
+db.create_all()
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
