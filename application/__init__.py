620f488a1bdc11b9de60b378079aefd826940bad
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2019-10-24 19:56:11 -0400

    set db for heroku

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ login_manager = LoginManager()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uugoazwybaczwi:7c8526f5010e90bf259421da375f1be793ab5a3f8bcead490b0a02437cbca0b3@ec2-54-243-208-234.compute-1.amazonaws.com:5432/da9779rd0a0i19'
     app.config['SECRET_KEY'] = 'blahblah'
     db.init_app(app)
     login_manager.init_app(app)
